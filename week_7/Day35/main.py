779552a76aa0240a1a52fe2eaf13869b05467fe0
Prachi-Mehta <prachimehta.ciphernutz@gmail.com>
2026-05-19 16:48:52 +0530

    Implemented streaming response endpoint

--- week_7/Day35/main.py
+++ week_7/Day35/main.py
@@ -13,3 +13,22 @@ llm = ChatGoogleGenerativeAI(
     model="gemini-3.1-flash-lite",
     google_api_key=os.getenv("GOOGLE_API_KEY")
 )
+
+class ChatReq(BaseModel):
+    question: str
+
+# streaming Generator
+def generate_res(question):
+    res = llm.stream(question)
+
+    for chunk in res:
+        if chunk.content:
+            yield str(chunk.content)
+
+# streaming endpoint
+@app.post("/chat")
+def chat(req:ChatReq):
+    return StreamingResponse(
+        generate_res(req.question),
+        media_type="text/plain"
+    )
